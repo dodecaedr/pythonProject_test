b7625aa714c717031497533e99afcb1722729058
Максим Пак <dodecaedr2000@gmail.com>
2025-09-21 01:12:22 +0400

    add test function add_numbers, is_even, find_max, x, divide

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,21 +1,21 @@
-def add_numbers(a: int, b: int) -> int:
-    return a + b
+from src.main import find_max, is_even, add_numbers, divide
 
 
-def is_even(a: int) -> int:
-    return a % 2 == 0
-
-
-def find_max(a: list) -> int:
-    if len(a) > 0:
-        return max(a)
-    return 0
-
-
-if __name__ == '__main__':
+def test_add_numbers():
     assert add_numbers(2, 3) == 5
 
+
+def test_is_even():
     assert is_even(2) == True
 
+
+def test_find_max():
     assert find_max([1, 2, 3, 4, 5]) == 5
+
     assert find_max([]) == 0
+
+
+def test_divide():
+    assert divide(2, 1) == 2
+
+    assert divide(2, 2) == 1
